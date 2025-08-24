d906cdbaf7163be6b279c0b7861781a91624181c
VuNam002 <namvu7702@gmail.com>
2025-08-24 18:19:06 +0700

    Sửa file main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from 'react-toastify'; // Added
 import 'react-toastify/dist/ReactToastify.css'; // Added
+import { AuthProvider } from "./context/AuthContext.tsx"; // Import AuthProvider
 
 const container = document.getElementById("root");
 if (container) {
@@ -12,7 +13,9 @@ if (container) {
   root.render(
     <StrictMode>
       <BrowserRouter>
-        <App />
+        <AuthProvider> {/* Wrap your app with AuthProvider */}
+          <App />
+        </AuthProvider>
         <ToastContainer /> {/* Added */}
       </BrowserRouter>
     </StrictMode>
