7df6047a418deb015cb23dc731cb22038f5f510d
VuNam002 <namvu7702@gmail.com>
2025-09-23 21:40:31 +0700

    sửa detail

--- frontend/src/pages/admin/user/detail.tsx
+++ frontend/src/pages/admin/user/detail.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { useUserClient } from '../../../hooks/userAdmin'; // Adjust import path as needed
+import { useUserClient } from '../../../hooks/userAdmin'; 
 
 interface UserClient {
     id: string;
