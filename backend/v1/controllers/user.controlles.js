6bbb5d79c0ecf35739ef244ef92b496cc3aa517e
VuNam002 <namvu7702@gmail.com>
2025-09-30 14:54:01 +0700

    sửa logic tên người bình luận

--- backend/v1/controllers/user.controlles.js
+++ backend/v1/controllers/user.controlles.js
@@ -17,7 +17,7 @@ module.exports.register = async (req, res, next) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const user = await User.create({ name, email, password: hashedPassword });
+    const user = await User.create({ fullName: name, email, password: hashedPassword });
 
     res.status(201).json({
       success: true,
